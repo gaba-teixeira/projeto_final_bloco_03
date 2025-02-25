f036983cca334de153596bc5cc3c12b8d9ae7d66
Gabriela Teixeira <gaba.andrade@outlook.com>
2025-02-25 11:24:34 -0300

    :sparkles: CRUD completo do componente Categoria

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/navbar/NavBar";
 import Home from "./pages/home/Home";
 import Footer from "./components/footer/Footer";
+import ListarCategoria from "./components/categoria/listarcategoria/ListarCategoria";
+import FormCategoria from "./components/categoria/formcategoria/FormCategoria";
+import DeletarCategoria from "./components/categoria/deletarcategoria/DeletarCategoria";
 
 function App() {
   return (
@@ -10,7 +13,10 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/categorias" element={<ListarCategoria />} /> */}
+          <Route path="/categorias" element={<ListarCategoria />} />
+          <Route path="/cadastrarcategoria" element={<FormCategoria />} />
+          <Route path="/editarcategoria/:id" element={<FormCategoria />} />
+          <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
         </Routes>
         <Footer />
       </BrowserRouter>
---
+++ src/components/categoria/deletarcategoria/DeletarCategoria.tsx
@@ -0,0 +1,90 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Categoria from "../../../models/Categoria";
+import { buscar, deletar } from "../../../services/Service";
+import { RotatingLines } from "react-loader-spinner";
+
+function DeletarCategoria() {
+  const navigate = useNavigate();
+
+  const [categoria, setCategoria] = useState<Categoria>({} as Categoria);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const { id } = useParams<{ id: string }>();
+
+  async function buscarPorId(id: string) {
+    try {
+      await buscar(`/categorias/${id}`, setCategoria);
+    } catch (error: any) {
+      alert("Erro");
+    }
+  }
+
+  useEffect(() => {
+    if (id !== undefined) {
+      buscarPorId(id);
+    }
+  }, [id]);
+
+  function retornar() {
+    navigate("/categorias");
+  }
+
+  async function deletarCategoria() {
+    setIsLoading(true);
+    try {
+      await deletar(`categorias/${id}`);
+      alert("Categoria deletado com sucesso");
+    } catch (error: any) {
+      alert("Erro ao excluir categoria!");
+    }
+    setIsLoading(false);
+    retornar();
+  }
+
+  return (
+    <div className="container w-1/3 mx-auto mt-8">
+      <div className="">
+        <h1 className="text-4xl text-center font-poppins my-4">Deletar Categoria</h1>
+        <p className="text-center font-semibold mb-4">
+          Você tem certeza de que deseja apagar a categoria a seguir?
+        </p>
+      </div>
+      <div className="border font-poppins flex flex-col rounded-2xl overflow-hidden justify-between w-md mt-8 m-2">
+        <header className="py-2 px-6 bg-purple-900 text-white font-bold text-xl">
+          Categoria
+        </header>
+        <p className="p-6 text-2xl bg-pink-200 h-full">{categoria.nome}</p>
+        <div className="flex">
+          <button
+            className="text-slate-100 bg-pink-400 hover:bg-pink-900 w-full 
+                    flex items-center justify-center font-code cursor-pointer"
+            onClick={retornar}
+          >
+            Não
+          </button>
+
+          <button
+            className="w-full text-slate-100 bg-purple-400 hover:bg-purple-950 
+                        flex items-center justify-center py-2 cursor-pointer font-code"
+            onClick={deletarCategoria}
+          >
+            {isLoading ? (
+              <RotatingLines
+                strokeColor="white"
+                strokeWidth="5"
+                animationDuration="0.75"
+                width="24"
+                visible={true}
+              />
+            ) : (
+              <span>Sim</span>
+            )}
+          </button>
+        </div>
+      </div>
+    </div>
+  ); 
+}
+
+export default DeletarCategoria;
